0958770d2d4f6dc6bf0317dcb5acee51ea0ed77b
orgih <orgih@mail.com>
2020-05-16 01:41:50 +0900

    Ready to update.

---
+++ mkdocs_with_pdf/themes/material-polyfills/typings/webpack-modules.d.ts
@@ -0,0 +1,1 @@
+declare module 'webpack-fix-style-only-entries';
---
+++ mkdocs_with_pdf/themes/material-polyfills/webpack.config.ts
@@ -0,0 +1,142 @@
+import ImageminPlugin from "imagemin-webpack-plugin"
+import MiniCssExtractPlugin = require("mini-css-extract-plugin")
+import FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries")
+import * as path from "path"
+import { Configuration } from "webpack"
+
+/* ----------------------------------------------------------------------------
+ * Helper functions
+ * ------------------------------------------------------------------------- */
+
+/**
+ * Webpack base configuration
+ *
+ * @param args - Command-line arguments
+ *
+ * @return Webpack configuration
+ */
+function config(args: Configuration): Configuration {
+  return {
+    mode: args.mode,
+
+    /* Loaders */
+    module: {
+      rules: [
+        /* SASS stylesheets */
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                url: false,
+                sourceMap: true
+              }
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                ident: "postcss",
+                plugins: () => [
+                  require("autoprefixer")(),
+                  require("postcss-inline-svg")({
+                    paths: [path.resolve(__dirname, "node_modules")],
+                    encode: false
+                  }),
+                  require("postcss-svgo")({
+                    plugins: [
+                      { removeDimensions: true },
+                      { removeViewBox: false }
+                    ],
+                    encode: false
+                  })
+                ],
+                sourceMap: true
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                implementation: require("sass"),
+                sassOptions: {
+                  includePaths: [
+                    "node_modules/modularscale-sass/stylesheets",
+                    "node_modules/material-design-color"
+                  ]
+                },
+                sourceMap: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+
+    /* Module resolver */
+    resolve: {
+      modules: [__dirname, path.resolve(__dirname, "node_modules")]
+    },
+
+    /* Plugins */
+    plugins: [],
+
+    /* Source maps */
+    devtool: args.mode === "production" ? "source-map" : "eval",
+
+    /* Filter false positives and omit verbosity */
+    stats: {
+      entrypoints: false,
+      warningsFilter: [/export '.[^']+' was not found in/]
+    }
+  }
+}
+
+/* ----------------------------------------------------------------------------
+ * Configuration
+ * ------------------------------------------------------------------------- */
+
+/**
+ * Webpack configuration
+ *
+ * @param env - Webpack environment arguments
+ * @param args - Command-line arguments
+ *
+ * @return Webpack configurations
+ */
+export default (_env: never, args: Configuration): Configuration[] => {
+  const base = config(args)
+  return [
+    /* Application */
+    {
+      ...base,
+      entry: {
+        "material-polyfills": "src/stylesheets/polyfills.scss"
+      },
+      output: {
+        path: path.resolve(__dirname, "dist")
+      },
+
+      /* Plugins */
+      plugins: [
+        ...base.plugins,
+
+        /* Stylesheets */
+        new FixStyleOnlyEntriesPlugin(),
+        new MiniCssExtractPlugin({
+          filename: `[name].css`
+        }),
+
+        /* Minify SVGs */
+        new ImageminPlugin({
+          svgo: {
+            plugins: [{ removeDimensions: true }, { removeViewBox: false }]
+          }
+        })
+      ],
+
+      /* Optimizations */
+      optimization: {}
+    }
+  ]
+}
